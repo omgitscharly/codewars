a603a25834a004ed662db5ef0a32d9db36c06608
Carlos Gámez <hardpx@gmail.com>
2022-04-12 11:36:49 -0500

    completed

---
+++ 068-8kyu-total-pressure-calculation.js
@@ -0,0 +1,3 @@
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+    return (((givenMass1/molarMass1) + (givenMass2/molarMass2)) * (0.082 * (temp + 273.15)))/volume;
+}   
