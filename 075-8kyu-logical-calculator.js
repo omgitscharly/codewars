661e606e45ef64d575b5e30b987203d11971a56d
Carlos Gámez <hardpx@gmail.com>
2022-04-19 11:49:13 -0500

    completed

---
+++ 075-8kyu-logical-calculator.js
@@ -0,0 +1,15 @@
+function logicalCalc(array, op){
+    var result = array[0];
+    for(var i = 1; i < array.length; i++) {
+    if(op == "AND") {
+    result = result && array[i];
+    }
+    if(op == "OR") {
+    result = result || array[i];
+    }
+    if(op == "XOR") {
+    result = result != array[i];
+    }
+    }
+    return result;
+}
