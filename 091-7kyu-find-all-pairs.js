3bb587bb15b86d23f2188b92e027460b16911255
Carlos Gámez <hardpx@gmail.com>
2022-05-06 17:11:53 -0500

    completed

---
+++ 091-7kyu-find-all-pairs.js
@@ -0,0 +1,12 @@
+function duplicates(array){
+    let count = 0
+    let newArray = array.slice().sort();
+    for (let i = 0; i < newArray.length; i++){
+        if (newArray [i] === newArray[i + 1]){
+          count++
+          newArray[i] = ''
+          newArray[i + 1] = ''
+        }
+      }
+    return count
+  }
