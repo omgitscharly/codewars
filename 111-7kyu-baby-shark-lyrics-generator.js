77054ea69869948f1ee5b4bfac140848c663c7ec
Carlos Gámez <hardpx@gmail.com>
2022-07-15 21:25:52 -0500

    completed

---
+++ 111-7kyu-baby-shark-lyrics-generator.js
@@ -0,0 +1,7 @@
+function babySharkLyrics(){
+    s=" shark"
+    fm=["Baby","Mommy","Daddy","Grandma","Grandpa"]
+    d=`,${" doo".repeat(6)}`
+    h="Let's go hunt"
+    return fm.reduce((l,n,i)=>l+=(n+s+d+"\n").repeat(3)+n+s+"!\n","")+(h+d+"\n").repeat(3)+h+"!\n"+"Run away,…"
+  }
