a8f5f30c75f3da1f73ba86b4a2ff96eebf5ed6c8
Carlos Gámez <hardpx@gmail.com>
2022-07-22 09:42:52 -0500

    kata completed

---
+++ 8kyu-third-angle-of-a-triangle.js
@@ -0,0 +1,15 @@
+//two numbers that are angles of a triangle
+
+//return a number that is th third angle
+
+function otherAngle(a, b) {
+    //return the differrence beween a + b and 180
+    return 180 - (a + b);
+  }
+  
+  otherAngle(10, 20)
+  
+  // (30, 60), 90
+  // (60, 60), 60
+  // (43, 78), 59
+  // (10, 20), 150
